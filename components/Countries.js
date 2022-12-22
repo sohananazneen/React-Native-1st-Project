8fe92ec4044f0556e29aa742ca62023b7c5a8b76
SohanaN <sohananaz01@gmail.com>
2022-12-22 20:45:50 +0600

    first commit

---
+++ components/Countries.js
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React, { useEffect, useState } from 'react'
+
+export default function Countries() {
+    const [countries, setCountries] = useState([]);
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+            .then(res => res.json())
+            .then(data => setCountries(data));
+    }, [])
+    return (
+        <View>
+            <Text>Countries:{countries.length}</Text>
+
+        </View>
+    )
+}
