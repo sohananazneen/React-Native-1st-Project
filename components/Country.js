1d049158eedb4008733c4006ab55e3a4365eed6f
SohanaN <sohananaz01@gmail.com>
2023-05-14 02:46:31 +0600

    Image showed and serach countries

--- components/Country.js
+++ components/Country.js
@@ -1,10 +1,17 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 
 export default function Country({ country }) {
     return (
         <View>
-            <Text>SingleCountry: {country.name.common}</Text>
+            <Text style={{ fontSize: 20 }}>SingleCountry: {country.name.common}</Text>
+            <Image
+                source={{
+                    uri: country.flags.png
+                }}
+                style={{ width: 200, height: 200 }}
+            >
+            </Image>
         </View>
     )
 }
